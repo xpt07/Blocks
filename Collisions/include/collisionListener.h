a18c6d145225f8902a406ab915538e55d8c915a3
xpt07 <101298390+xpt07@users.noreply.github.com>
2023-06-23 21:47:33 +0100

    Update collisionListener.h

--- Collisions/include/collisionListener.h
+++ Collisions/include/collisionListener.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<box2d/box2d.h>
 #include "PhysicalObject.h"
+#include <string>
 
 // Forward declaration of the Scene class
 class Scene;
@@ -20,7 +21,15 @@ public:
 	// Setter function to set the pointer to the current Scene object
 	void setScene(Scene* pScene) { m_scene = pScene; };
 
+	inline std::string getCollisionCount()
+	{
+		return "Number of Collisions: " + std::to_string(m_collisionCount - 1);
+	};
+
+
 private:
 	//Pointer to the Scene object
 	Scene* m_scene = nullptr;
+
+	int m_collisionCount = 0;
 };
